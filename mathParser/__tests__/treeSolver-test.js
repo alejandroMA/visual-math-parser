3dc2e2858c8d142c0b2c278d92b90f5973db6588
Alejandro Madariaga Angeles <amndell@gmail.com>
2016-05-19 21:47:08 -0500

    first version of treeSolver

---
+++ mathParser/__tests__/treeSolver-test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+jest.unmock('../treeSolver');
+jest.unmock('../queryParser');
+jest.unmock('../../structures/node');
+
+describe('treeSolver', function() {
+    const treeSolver = require('../treeSolver');
+    const queryParser = require('../queryParser');
+    let tree;
+
+    beforeEach(function() {
+        tree = undefined;
+    });
+
+    it('resolves tree from "2+4*12/3-1"', function() {
+        tree = queryParser('2+4*12/3-1');
+
+        expect(treeSolver(tree)).toEqual(17);
+    });
+
+    it('resolves tree from "2^8 * 4 - 20"', function() {
+        tree = queryParser('2^8 * 4 - 20');
+
+        expect(treeSolver(tree)).toEqual(1004);
+    });
+});
---
+++ mathParser/treeSolver.js
@@ -0,0 +1,46 @@
+'use strict';
+
+
+function treeSolver(tree) {
+
+    return solver(tree);
+
+    function solver(tree) {
+        if ((tree.leftChild !== undefined) && (tree.rightChild !== undefined)) {
+            let symbol = tree.data;
+            let leftNumber = treeSolver(tree.leftChild);
+            let rightNumber = treeSolver(tree.rightChild);
+
+            return mathOperation(symbol, rightNumber, leftNumber);
+        } else {
+            return tree.data;
+        }
+    }
+
+    function mathOperation(symbol, rightNumber, leftNumber) {
+        let result = undefined;
+
+        switch (symbol) {
+            case '+':
+                result = leftNumber + rightNumber;
+                break;
+            case '-':
+                result = leftNumber - rightNumber;
+                break;
+            case '*':
+                result = leftNumber * rightNumber;
+                break;
+            case '/':
+                result = leftNumber / rightNumber;
+                break;
+            case '^':
+                result = Math.pow(leftNumber, rightNumber);
+                break;
+        }
+
+        return result;
+    }
+    
+}
+
+module.exports = treeSolver;
